bab5bf2d8f2fe8b33e264a843060e328df174fa1
Horvath Gergo <gergohs@gmail.com>
2018-12-14 14:00:17 +0100

    Find the missing number: solution using Collections.shuffle

---
+++ find_missing_number_2.0/findMissingNum.java
@@ -0,0 +1,29 @@
+import java.util.*;
+/* Steps:
+#1: ArrayList to store mixed numbers from 1 to 20 - the ArrayList can be expandable
+#2: Shuffling the original list
+#3: Calculate the sum of the elements in the ArrayList
+#4: Remove an element from the list (6 is just a sample number)
+#5: Find the missing element, using for:each statement to iterate through the ArrayList
+*/
+public class findMissingNum {
+    public static void main(String[] args) {
+// #1
+        ArrayList<Integer> listForCheck = new ArrayList<>(
+        Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20));
+        System.out.println("The original list: " + listForCheck + "\n");
+// #2
+        Collections.shuffle(listForCheck);
+        System.out.println("The shuffled list: " + listForCheck);
+// #3
+        int sumOfElements = (listForCheck.size() * (listForCheck.size() + 1)) / 2;
+// #4
+        listForCheck.remove(listForCheck.size() - 8);
+        System.out.println("Remove an element: " + listForCheck + "\n");
+// #5
+        for (Integer elementOfList : listForCheck) {
+            sumOfElements -= elementOfList;
+        }
+        System.out.println("* * * * * The missing number is: " + sumOfElements + " * * * * *");
+    }
+}
